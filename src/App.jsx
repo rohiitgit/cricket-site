b4abe0d4ab7c1b0f9deee841d30e1607bb7c5dfd
rohiitgit <rohitcodes03@gmail.com>
2024-09-25 04:35:11 +0530

    improved the slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 // src/App.jsx
 import "./App.css"; // If you have custom styles
 import TextReveal from "./components/text-reveal";
-import ImageSlider from "./components/image-slider.jsx";
 import HeroPage from "./components/hero.jsx";
+import Slider from "./components/Slider.jsx";
 
 function App() {
   return (
     <>
       <HeroPage />
       <TextReveal />
-      <ImageSlider />
+      <Slider />
     </>
   );
 }
