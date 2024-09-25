95c64918646a401f074177138d4eb9a412e32b63
rohiitgit <rohitcodes03@gmail.com>
2024-09-26 00:41:40 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
-// app.jsx
 import TextReveal from './components/TextReveal';
 import NewClub from './components/NewClub';
-import Hero from './components/Hero'
-import Slider from './components/Slider'
-import TextRevel2 from './components/TextRevel2'
+import Hero from './components/Hero';
+import Slider from './components/Slider';
+import TextRevel2 from './components/TextRevel2';
 import SportsArticles from './components/SportsArticle';
 import Footer from './components/Footer';
+import RegForm from './components/RegForm';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="flex flex-col space-y-8">
-      <Hero className="block " /> 
-      <TextRevel2 className="block"/>
-      <TextReveal className="block " />    
-      <Slider className="block" /> 
-      <NewClub className="block" /> 
-      <SportsArticles className='block' />
-      <Footer className='block'/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Hero />
+            <TextRevel2 />
+            <TextReveal />
+            <Slider />
+            <NewClub />
+            <SportsArticles />
+            <Footer />
+          </>
+        } />
+        <Route path="/register" element={<RegForm />} />
+      </Routes>
+    </Router>
   );
 }
 
